510a38b2be7c3f18a3549ed010d0a29777f3e4d6
christhecoolguy <christopherdiggs1@gmail.com>
2023-05-09 19:06:28 -0700

    argV3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-def reverse_string(string):
-    if string == "":
-        return ""
-    elif string == "Hello":
-        return "olleH"
-    else:
-        return string[::-1]
+def capitalize_string(s):
+    return s.capitalize()
 
-print(reverse_string("hello")) # Output: olleh
-print(reverse_string("world")) # Output: dlrow
-print(reverse_string("")) # Output: 
-print(reverse_string("Hello")) # Output: olleH
+if __name__ == "__main__":
+    string = "hello"
+    capitalized = capitalize_string(string)
+    print(capitalized)
+
