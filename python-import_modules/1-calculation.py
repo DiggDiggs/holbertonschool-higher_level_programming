394274bfc6cd810470e522d42b232bfaae04d495
christhecoolguy <christopherdiggs1@gmail.com>
2023-05-09 19:33:13 -0700

    calPEP8

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,9 +2,7 @@
 if __name__ == "__main__":
     a = 10
     b = 5
-    
     from calculator_1 import add, sub, mul, div
-    
     print("{0} + {1} = {2}".format(a, b, add(a, b)))
     print("{0} - {1} = {2}".format(a, b, sub(a, b)))
     print("{0} * {1} = {2}".format(a, b, mul(a, b)))
