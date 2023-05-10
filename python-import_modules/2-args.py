58338df0f8114ef892453d11bec0c55ca5faf1e6
christhecoolguy <christopherdiggs1@gmail.com>
2023-05-10 12:16:33 -0700

    argsstill

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    n = len(sys.argv)
-    hank = 0
+# Write a program that prints the number of and the list of its arguments.
+from sys import argv
 
-    for i in range(1, n):
-        hank += int(sys.argv[i])
-    print("{}".format(hank))
+__name__ == '__main__'
+if len(argv) == 1:
+    print("{} arguments.".format(len(argv) - 1))
+elif len(argv) == 2:
+    print("{} argument:".format(len(argv) - 1))
+else:
+    print("{} arguments:".format(len(argv) - 1))
+for i in range(1, len(argv)):
+    print("{}: {}".format(i, argv[i]))
