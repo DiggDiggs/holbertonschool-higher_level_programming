e6274d35449be148e12278de463516a25133a475
christhecoolguy <christopherdiggs1@gmail.com>
2023-05-10 12:19:53 -0700

    hidden

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+# Write a program that prints the result of the addition of all arguments
+if __name__ == '__main__':
     import sys
-    n = len(sys.argv)
-    hank = 0
-    for i in range(1, n):
-        hank += int(sys.argv[i])
-    print("{}".format(hank))
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
