779e8c522cfb0bb36afe0188fdbbe3a6144f3079
christhecoolguy <christopherdiggs1@gmail.com>
2023-05-15 19:40:04 -0700

    7v3add

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
-def add_tuple(tuple_a=(), tuple_b=()):
-    a = tuple_a + (0, 0)
-    b = tuple_b + (0, 0)
-    return (a[0] + b[0], a[1] + b[1])
+def add_tuples(a, b):
+    x = a[0] + b[0]
+    y = a[1] + b[1]
+    return (x, y)
 
 
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print("[{}]".format(", ".join(str(i) for i in new_tuple)))
+result = add_tuples((1, 2), (3, 4))
+print(result)
