c24880ccb5a2ad83c2294121c588548ffbe70c3f
christhecoolguy <christopherdiggs1@gmail.com>
2023-05-17 19:07:51 -0700

    fix_done

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+
+    highscore = 0
+    theKey = None
+
+    for key, value in a_dictionary.items():
+        if value > highscore:
+            highscore = value
+            theKey = key
+    return theKey
