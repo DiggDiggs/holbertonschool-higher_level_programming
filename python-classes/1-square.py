fffc5bd13ab536f28139189a1c4b178222fe00fd
christhecoolguy <christopherdiggs1@gmail.com>
2023-05-18 10:44:04 -0700

    module_documented

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,5 +2,15 @@
 
 
 class Square:
+    """
+    A class that represents a square.
+    Attribuyes:
+        --size (int): THE size of the square.
+    """
     def __init__(self,size):
+        """
+        Initialize a new instance of the square.
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
