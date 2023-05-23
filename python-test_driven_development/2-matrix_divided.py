24280f0ffd7a10ff154f2abb52845f65b0427ab0
christhecoolguy <christopherdiggs1@gmail.com>
2023-05-23 14:05:27 -0700

    fix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
-
-"""This module defines a matrix division function.
-Matrix: A list of lists of ints or floats
-div: What to divide by
-Return: New divided matrix"""
+"""2-matrix_divided.py and tests/2-matrix_divided.txt"""
 
 
 def matrix_divided(matrix, div):
-    """Divide all elements of a matrix.
-    Raises TypeErrors if:
-        Matrix contains non-numbers
-        Matrix contains rows of different sizes
-        Div is not an int or float
-    Raises ZeroDivisionError if:
-        Div is 0"""
-    if (not isinstance(matrix, list) or matrix == [] or
-            not all(isinstance(row, list) for row in matrix) or
-            not all((isinstance(element, int) or isinstance(element, float))
-                    for element in [num for row in matrix for num in row])):
-        raise TypeError("matrix must be a matrix (list of lists) of "
-                        "integers'floats")
-
-    if not all(len(row) == len(matrix[0]) for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-
-    if not isinstance(div, int) and not isinstance(div, float):
+    """
+    divides all elements of a matrix, elements must be ->
+        int or float type
+    """
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-
-    if div == 0:
+        return matrix
+    elif div == 0:
         raise ZeroDivisionError("division by zero")
+        return matrix
 
-    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
+    prevRowLen = -1
+    new_list = []
+    for row in matrix:
+        if (prevRowLen != len(row) and prevRowLen != -1):
+            raise TypeError("Each row of the matrix must have the same size")
+            return matrix
+        for ele in row:
+            if not isinstance(ele, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of" +
+                                " integers/floats")
+                return matrix
+            else:
+                new_list.append(round(ele / div, 2))
+        prevRowLen = len(row)
+
+    return new_list
