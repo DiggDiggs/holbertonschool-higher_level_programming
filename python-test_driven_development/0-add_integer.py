163a6be6128fb3e093f41ae315473c6734d3b0aa
christhecoolguy <christopherdiggs1@gmail.com>
2023-05-22 21:42:52 -0700

    0addtxt

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,14 +3,12 @@
 
 
 def add_integer(a, b=98):
-    """
-    adds an interger
-    to test/0-add_integer.txt
-    check that error type
-    """
-    if not isinstance(b, int):
-        if not isinstance(b, float):
-            b = int(b)
-        else:
-            raise typeError("b must be an integer")
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or float")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer or float")
+
+    a = int(a)
+    b = int(b)
+
     return a + b
