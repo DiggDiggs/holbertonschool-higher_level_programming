af329fd9bd9586164240ce2a7a8b572a3fb8effa
christhecoolguy <christopherdiggs1@gmail.com>
2023-05-23 13:34:38 -0700

    commit

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
 
-def matrix_divided(matrix, div):
-    # Check if matrix is a list of lists of integers or floats
-    if not isinstance(matrix, list) or any(
-        not isinstance(row, list) for row in matrix
-    ):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats"
-        )
+"""This module defines a matrix division function.
+Matrix: A list of lists of ints or floats
+div: What to divide by
+Return: New divided matrix"""
 
-    # Check if each row of the matrix has the same size
-    row_size = len(matrix[0])
-    if any(len(row) != row_size for row in matrix):
+
+def matrix_divided(matrix, div):
+    """Divide all elements of a matrix.
+    Raises TypeErrors if:
+        Matrix contains non-numbers
+        Matrix contains rows of different sizes
+        Div is not an int or float
+    Raises ZeroDivisionError if:
+        Div is 0"""
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(element, int) or isinstance(element, float))
+                    for element in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers'floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-    # Check if div is a number (integer or float)
-    if not isinstance(div, (int, float)):
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
 
-    # Check if div is not zero
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    # Divide each element of the matrix by div and round to 2 decimal places
-    new_matrix = [
-        [round(element / div, 2) for element in row]
-        for row in matrix
-    ]
-
-    return new_matrix
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
