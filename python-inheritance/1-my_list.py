295fc3ee90c3fffe87008f7ed5938db18d62a3db
christhecoolguy <christopherdiggs1@gmail.com>
2023-05-28 17:49:06 -0700

    class_fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+# Write a class MyList that inherits from list
+"""This is a print_sorted method that prints the list
+but sorted (ascending sort)"""
+
+
 class MyList(list):
+    """MyList class inherits from list"""
     def print_sorted(self):
+        """prints list"""
         sorted_list = sorted(self)
         print(sorted_list)
