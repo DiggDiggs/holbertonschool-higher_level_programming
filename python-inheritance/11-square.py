f0f0ea9ce2f976dbc1db5f786ebe8cce6bcfef8f
christhecoolguy <christopherdiggs1@gmail.com>
2023-05-28 18:31:34 -0700

    inheritance

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-"""
-Write a class Square that inherits from Rectangle (9-rectangle.py):
-
-Instantiation with size: def __init__(self, size)::
+"""Square class that inherits from Rectangle (9-rectangle.py)
+Instantiation with size: def __init__(self, size)
 size must be private. No getter or setter
 size must be a positive integer, validated by integer_validator
 the area() method must be implemented
-print() should print, and str() should return, the
-    square description: [Square] <width>/<height>
-"""
+print() should print, and str() should return, the square description:
+[Square] <width>/<height>"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """The square class based on rectangle"""
+    """Square class that inherits from Rectangle (9-rectangle.py)"""
     def __init__(self, size):
-        """This is the Initialization Method"""
-        super().__init__(size, size)
+        """Instantiation with size"""
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def __str__(self):
-        """return string representation of rectangle"""
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        """returning the string representation of the rectangle"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-"""
-Write a class Rectangle that inherits from BaseGeometry
-
-Instantiation with width and height: def __init__(self, width, height)::
+""" Rectangle class inherits from BaseGeometry class in 7-base_geometry.py
+Instantiation with width and height: def __init__(self, width, height):
 width and height must be private. No getter or setter
-width and height must be positive integers validated by integer_validator
-the area() method must be implemented
-print() should print, and str() should return, the following rectangle
-    description: [Rectangle] <width>/<height>
-"""
+width and height must be positive integers, validated by integer_validator
+the area() method must be implemented and print() should print, and str()
+should return, the following rectangle description:
+[Rectangle] <width>/<height>"""
+
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    This is a Rectangle class!
-    """
+    """Rectangle class inherits from BaseGeometry class"""
     def __init__(self, width, height):
-        """This is the Initialization Method"""
+        """Instantiation with width and height"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
-    def area(self):
-        """returns area of rectangle"""
-        return (self.__width * self.__height)
-
     def __str__(self):
-        """return string representation of rectangle"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        """returning the area of the rectangle"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+    def area(self):
+        """the area() method must be implemented"""
+        return self.__width * self.__height
