2315d24ea3542d3dd49c1dadd32232c4829246fd
DiggDiggs <christopherdiggs1@gmail.com>
2023-07-28 13:02:27 -0700

    amazingJS

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -2,7 +2,9 @@
 const argv = require('process').argv;
 
 if (argv.length < 3) {
-  console.log('no argument');
+  console.log('C is fun');
+  console.log('Python is cool');
+  console.log('JavaScript is amazing');
 } else if (argv.length === 3) {
   console.log('Argument found');
 } else {
