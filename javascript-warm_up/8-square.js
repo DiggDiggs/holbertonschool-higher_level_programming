69bd6752732cc77f6f01c7b41891b5148917864f
DiggDiggs <christopherdiggs1@gmail.com>
2023-07-30 16:14:07 -0700

    X

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,12 +3,12 @@
 const size = parseInt(process.argv[2]);
 
 if (isNaN(size) || size < 1) {
-    console.log('Invalid size positve number plz');
+    console.log('Missing size');
 } else {
     for (let i = 0; i < size; i++) {
         let row = '';
         for (let j = 0; j < size; j++) {
-            row += '*';
+            row += 'X';
         }
         console.log(row);
     }
