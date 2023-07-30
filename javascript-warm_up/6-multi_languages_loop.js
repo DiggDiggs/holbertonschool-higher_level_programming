7182d2245ce547cc28194514631ff0903027674e
DiggDiggs <christopherdiggs1@gmail.com>
2023-07-30 15:33:38 -0700

    up

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+let counter;
+const STRING_ARRAY = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (counter = 0; counter < STRING_ARRAY.length; counter++) {
+    console.log(STRING_ARRAY[counter]);
+}
