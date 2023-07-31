bf48f1abe10ef5fa730ab74c447fed7df2a640bf
DiggDiggs <christopherdiggs1@gmail.com>
2023-07-31 12:31:06 -0700

    fix

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
+const ARGV = process.argv;
+const num = parseInt(ARGV[2]);
 
-function factorial(n) {
-    if (isNaN(n) || n <= 0) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
+console.log(nFactorial(num));
+
+function nFactorial (n) {
+  if (n === 0 || n === 1 || isNaN(num)) {
+    return (1);
+  } else {
+    return (n * nFactorial(n - 1));
+  }
 }
-const number = parseInt(process.argv[2]);
-const result = factorial(number);
-console.log('Factorial of', number, 'is:', result);
