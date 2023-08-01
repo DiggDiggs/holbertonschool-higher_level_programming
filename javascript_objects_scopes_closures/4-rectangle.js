8029f44fb49ae8d5f925b7971d89a5f380441c6d
DiggDiggs <christopherdiggs1@gmail.com>
2023-08-01 12:42:13 -0700

    double/rotate

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+class Rectangle {
+    constructor(w, h) {
+      if (w <= 0 || h <= 0) {
+        return {};
+      } else {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  
+    print() {
+      if (this.width <= 0 || this.height <= 0) {
+        console.log('');
+      } else {
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
+      }
+    }
+  
+    rotate() {
+      [this.width, this.height] = [this.height, this.width];
+    }
+  
+    double() {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+  
+  module.exports = Rectangle;
+  
