388082461c8f4db5acf051332879f0ceee43e23b
DiggDiggs <christopherdiggs1@gmail.com>
2023-08-01 14:35:18 -0700

    fixes

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,24 +1,23 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0) {
-        // If w or h is not a positive integer or equal to 0, create an empty object
-        return {};
-      } else {
+    constructor (w, h) {
+      if (w >= 1 && h >= 1) {
         this.width = w;
         this.height = h;
       }
     }
   
-    print() {
-        if (this.width < 0 || this.height < 0) {
-            console.log('');
-        }else {
-            for (let i = 0; i < this.width; i++) {
-                console.log('X'.repeat(this.width));
-            }
-        }
+    print () {
+      let xCounter;
+      let yCounter;
+      let output = '';
+      for (xCounter = 0; xCounter < this.width; xCounter++) {
+        output += 'X';
+      }
+      for (yCounter = 0; yCounter < this.height; yCounter++) {
+        console.log(output);
+      }
     }
-}
-
-module.exports = Rectangle;
+  }
+  
+  module.exports = Rectangle;
