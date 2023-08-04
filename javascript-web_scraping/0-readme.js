94241c2b7376a17c18616e2b5d0f4d24a52b361a
DiggDiggs <christopherdiggs1@gmail.com>
2023-08-04 16:36:46 -0700

    0CIsSuperFun!

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+const ARGV = process.argv
+
+fs.readFile(ARGV[2], 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+    }
+        console.log(data);
+
+});
