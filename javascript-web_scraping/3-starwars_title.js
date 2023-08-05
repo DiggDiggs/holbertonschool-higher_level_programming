3c8b67bc2de25b3b3e232e75e270120f983cbfb2
DiggDiggs <christopherdiggs1@gmail.com>
2023-08-04 18:08:00 -0700

    starwars name order

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request.get(apiUrl, (err, response, body) => { 
+    if (err) {
+        console.error(err);
+    } else if (response.statusCode === 200) { 
+        const movieData = JSON.parse(body);
+        const movieTitle = movieData.title;
+        console.log(movieTitle);
+    } else {
+        console.error(`Error: Unexpected status code: ${response.statusCode}`);
+    }
+});
