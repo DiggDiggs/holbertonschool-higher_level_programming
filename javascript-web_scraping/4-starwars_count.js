eec2de1bc7a18546b34091a458c95470fa0287ce
DiggDiggs <christopherdiggs1@gmail.com>
2023-08-05 20:15:06 -0700

    wedgeAntilles

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+const characterId = 18;
+
+function countMoviesWithCharacter(data) {
+    if (!Array.isArray(data.characters)) {
+        return 0;
+    }
+
+    let count = 0;
+    for (const characterUrl of data.characters) {
+        const characterIdInUrl = characterUrl.split('/').slice(-2, -1)[0];
+        if (parseInt(characterIdInUrl) === characterId) {
+            count ++;
+        }
+    }
+    return count;
+}
+
+request.get(apiUrl, (err, response, body) => {
+    if (err) {
+        console.error(err);
+    } else if (response.statusCode === 200) {
+        const data = JSON.parse(body);
+        const numberOfMovies = countMoviesWithCharacter(data);
+        console.log(`Number of movies with "Wedge Antilles": ${numberOfMovies}`);
+    } else {
+        console.error(`Error: Unexpected status code ${response.statusCode}`);
+    }
+});
